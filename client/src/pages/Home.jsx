b61ed77b1566e56f641395155c3082a9fc5ef3fb
RimDallali <dallali.rim1@gmail.com>
2024-02-03 20:05:29 -0500

    broken code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,10 @@
-// import React from "react";
+import { NavLink } from "react-router-dom";
 import "./home.css";
-    
+
 function Home() {
     return (
         <div className="home">
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
-            <p> HOME </p>
+            <p> <li><NavLink className="navlink" to="/create-account">Create Accounts</NavLink></li> </p>
         </div>
     );
 }
